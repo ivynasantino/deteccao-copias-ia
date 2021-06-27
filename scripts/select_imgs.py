267da806a2ce65315f557ed38546b374c4fc8047
ivynasantino <ivyna.alves@ccc.ufcg.edu.br>
2021-06-27 19:06:25 -0300

    add scripts de gerar subconjunto de imagens

--- scripts/select_imgs.py
+++ scripts/select_imgs.py
@@ -1,22 +1,45 @@
 import random, os
 import glob
 import shutil
+import argparse
 
-path = "../../../../Downloads/img_align_celeba"
-dst_path = "../db/baseline/"
-
-random_filename = []
-for i in range(10):
-    random_filename.append(
-        random.choice(
-            [
-                path + "/" + x
-                for x in os.listdir(path)
-                if os.path.isfile(os.path.join(path, x))
-            ]
+def select_images(dst_path, num_imgs):
+    random_filename = []
+    for i in range(num_imgs):
+        random_filename.append(
+            random.choice(
+                [
+                    path + "/" + x
+                    for x in os.listdir(path)
+                    if os.path.isfile(os.path.join(path, x))
+                ]
+            )
         )
-    )
-print(random_filename)
+    print(random_filename)
 
-for p in random_filename:
-    shutil.copy(p, dst_path)
+    for p in random_filename:
+        shutil.copy(p, dst_path)
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Seleciona imagens de um dataset."
+    )
+
+    parser.add_argument(
+        "-p", "--caminho_destino", required=True, help="Caminho de destino das imagens selecionadas."
+    )
+    parser.add_argument(
+        "-n", "--numero_imagens", required=True, help="Número de imagens selecionadas."
+    )
+
+    return parser.parse_args()
+
+def run_select():
+    args = parse_args()
+    dst_path = args.caminho_destino
+    num_imgs = args.numero_imagens
+
+    select_images(dst_path, num_imgs)
+
+if __name__ == "__main__":
+    run_select()
